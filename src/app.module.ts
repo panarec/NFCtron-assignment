a283a77ff3103bfb63d63c7551fffaaecd5c6176
Pavol Vernarec <pver@paymorrow.com>
2024-08-23 13:45:32 +0200

    feat: database setup

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,15 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { DataModule } from './data/data.module';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from './database/database.module';
 @Module({
-  imports: [],
+  imports: [
+    DataModule,
+    ConfigModule.forRoot({ isGlobal: true }),
+    DatabaseModule,
+  ],
   controllers: [AppController],
-  providers: [AppService],
+  providers: [],
 })
 export class AppModule {}
