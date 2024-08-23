48ed57ba5b344270a5f8fa43c7b4fd9ba7a60104
Pavol Vernarec <pver@paymorrow.com>
2024-08-23 14:11:16 +0200

    feat: database seeding with fake data

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,11 +3,13 @@ import { AppController } from './app.controller';
 import { DataModule } from './data/data.module';
 import { ConfigModule } from '@nestjs/config';
 import { DatabaseModule } from './database/database.module';
+import { FakerModule } from './faker/faker.module';
 @Module({
   imports: [
     DataModule,
     ConfigModule.forRoot({ isGlobal: true }),
     DatabaseModule,
+    FakerModule,
   ],
   controllers: [AppController],
   providers: [],
--- src/database/database.service.ts
+++ src/database/database.service.ts
@@ -1,9 +1,27 @@
 import { Injectable, OnModuleInit } from '@nestjs/common';
 import { PrismaClient } from '@prisma/client';
+import { FakerService } from '../faker/faker.service';
 
 @Injectable()
 export class DatabaseService extends PrismaClient implements OnModuleInit {
+  constructor(private fakerService: FakerService) {
+    super();
+  }
+
   async onModuleInit() {
     await this.$connect();
+
+    // Check if the database is empty
+    const count = await this.customer.count();
+    if (count > 0) {
+      console.log('Database is not empty, skipping seeding');
+      return;
+    }
+
+    // Create fake customers and insert them into the database
+    const fakeCustomers = this.fakerService.createFakeCustomers(10);
+    await this.customer.createMany({
+      data: fakeCustomers,
+    });
   }
 }
